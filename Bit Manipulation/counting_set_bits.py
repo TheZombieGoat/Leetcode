a8b6fe5f3a9433b83e3cd95b2d1da4a610be0698
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-27 17:57:47 -0400

    Update counting_set_bits.py
    Implemented Brian Kernighan method. More efficient and clean.

--- Bit Manipulation/counting_set_bits.py
+++ Bit Manipulation/counting_set_bits.py
@@ -1,15 +1,13 @@
-"""O(n) implementation where n = number of bits.
-   Counts number of set bits.
-"""
+"""Counting bits. Brian Kernighan method. Only counts set bits."""
 def count_bits(num):
-	count = 0
-	while num >= 1:
-		count += num & 1
-		num = num >> 1
-	print(count)
+	c = 0 #c accumulates the total bits set in num
+	while num:
+ 		num &= num - 1 # clear the least significant bit set
+ 		c = c + 1
+	print(c)
 
 if __name__ == '__main__':
-	num = 0
+	num = int(input("What number do you want to count set bits of? (Enter -1 to exit) : "))
 	while num >= 0:
-		num = int(input("What number do you want to count set bits of? (Enter -1 to exit) : "))
 		count_bits(num)
+		num = int(input("What number do you want to count set bits of? (Enter -1 to exit) : "))
