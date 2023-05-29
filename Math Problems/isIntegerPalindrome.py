ec7d4a3dd1c046d6fea345a57f69b02086909850
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-05-29 00:00:37 -0400

    Update isIntegerPalindrome.py

--- Math Problems/isIntegerPalindrome.py
+++ Math Problems/isIntegerPalindrome.py
@@ -1,29 +1,10 @@
-class Solution:
-
-	def lop(n,digits):
-		sum_num = 0
-		half_dig = digits // 2
-		m = n
-		for i in range(half_dig):				
-			n = n // 10**(digits-1-i) #num_digits - 1 - i
-			sum_num = sum_num + n*10**i # -(i + 1)
-			m = m - n*10**(digits-1-i) #num_digits - 1 - i
-			n = m
-		return sum_num
-
-	def isPalindrome(self, num: int) -> bool:
-		if num < 0 or num == 10:
-			print("It's not a Palindrome.")
-			return False
-		elif num < 10:
-			print("It's a Palindrome")
-			return True
-
-		new_num = num
-		digits = 1
-
-		while num >= 10:
-			num = num // 10
-			digits += 1
-		return Solution.lop(new_num,digits) == new_num % 10**(digits//2)
+def isPalindrome(num):
+	if num < 0:
+		return False
+	y = 0
+	q = num
+	while q != 0:
+		y = 10 * y + q % 10
+		q //= 10
+	return num == y 
 
