a9294b8f4ac83538302482aedf0929d5218468da
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-02 20:36:48 -0400

    Create squares_of_sorted_array.py

---
+++ Array/squares_of_sorted_array.py
@@ -0,0 +1,4 @@
+#Given an integer array nums sorted in non-decreasing order, return an array of the squares of each number sorted in non-decreasing order.
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        return sorted([i**2 for i in nums])
