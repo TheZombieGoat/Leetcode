b5aa328463f74a91e3f955138e6acc338d9e2f47
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-02 21:27:44 -0400

    Update majority_element.py

--- Array/majority_element.py
+++ Array/majority_element.py
@@ -1,21 +1,16 @@
 #Given an array nums of size n, return the majority element.
 #The majority element is the element that appears more than (n / 2) times. You may assume that the majority element always exists in the array.
 
- 
+#O(n) time 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        nums.sort()
-        counter = 0
-        m = 0
-        res = 0
-        if len(nums) < 3:
-            return nums[0]
-        for i in range(len(nums)-1):
-            if nums[i+1] == nums[i]:
-                counter += 1
+        count = 0
+        result = 0
+        for n in nums:
+            if count==0:
+                result=n
+            if n==result:
+                count+=1
             else:
-                counter = 0
-            if counter > m:
-                m = counter
-                res = nums[i]
-        return res
+                count-=1
+        return result
