4016e732caf752420740ac6983acb990df06d2d7
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-04 00:39:54 -0400

    Update unique_path.py

--- Dynamic_Programming/unique_path.py
+++ Dynamic_Programming/unique_path.py
@@ -1,19 +1,17 @@
-#return num of unique paths
+"""
+There is a robot on an m x n grid. The robot is initially located at the top-left corner (i.e., grid[0][0]). The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move either down or right at any point in time.
 
-class Solution:
-    def uniquePaths(m: int, n: int) -> int:
-        d = {}
-        d[0] = 0
-        def up(a,b):
-            if a == 1 and b == 1:
-                d[0] = d[0] + 1
-            else:
-                if a > 0:
-                    up(a-1,b)
-                if b > 0:
-                    up(a,b-1)
-        up(m,n)
-        return d[0]
-        
+Given the two integers m and n, return the number of possible unique paths that the robot can take to reach the bottom-right corner.
 
-    print(uniquePaths(3,7))
+The test cases are generated so that the answer will be less than or equal to 2 * 109.
+"""
+
+class Solution:    
+    def uniquePaths(self,m: int, n: int) -> int:
+        return up(m,n)
+@cache
+def up(a,b):
+    if a == 1 or b == 1:
+        return 1 
+    else:
+        return up(a-1,b) + up(a,b-1) 
