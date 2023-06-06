386e1cea15ed91e4126f5a5cbf8e6c81a4788d8f
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-05 23:59:23 -0400

    Update Is_palindrome.py

--- Strings/Is_palindrome.py
+++ Strings/Is_palindrome.py
@@ -6,8 +6,16 @@ Given a string s, return true if it is a palindrome, or false otherwise.
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        temp = ""
-        for c in s:
-            if c.isalpha() or c.isnumeric():
-                temp += c
-        return temp.lower() == temp[::-1].lower() 
+        s = s.lower()
+        i,j = 0, len(s)-1
+        while i < j:
+            if not s[i].isalnum():
+                i += 1
+            if not s[j].isalnum():
+                j -= 1
+            if s[i].isalnum() and s[j].isalnum():
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+        return True
