4b76bf6f0c9c43c305d49e68be37638ebfece8f7
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-05 21:23:53 -0400

    Update maximum_subarray.c
    reduced space complexity from O(n) to O(1)

--- Dynamic_Programming/maximum_subarray.c
+++ Dynamic_Programming/maximum_subarray.c
@@ -4,45 +4,15 @@
 
 int maxSubArray(int arr[], int narr)
 {
-    if(narr > 1){
-        int i;
-        int j = 0;
-        int *p = (int*)malloc(sizeof(int)*narr);
-        int sum = 0;
-        bool pos = false;
-        for(i = 0; i < narr; i++){
-            if(arr[i] > 0){
-                sum = arr[i];
-                p[j] = sum;
-                j++;
-                pos = true;
-            }
-            while(sum > 0 && i < narr-1){
-                i++;
-                sum += arr[i];
-                p[j] = sum;
-                j++;
-            }
-            sum = 0;
-        }
-        if(!pos){
-            int g = arr[0];
-            for (int f = 1; f < narr; f++){
-                 if(arr[f] > g)
-                    g = arr[f];
-            }
-                return g;
-        }    
-        int k = 0;
-        int x = p[k];
-        for (k = 1; k < j; k++)
-        {
-            if(p[k] > x)
-            x = p[k];
-        }
-        free(p);
-        return x;
-    }else
-        return arr[0];
+    int currSum = 0;
+    int maxSum = arr[0];
+    for(int i = 0; i < narr; i++){
+        currSum += arr[i];
+        if(currSum > maxSum)
+            maxSum = currSum;
+        if(currSum < 0)
+            currSum = 0;
+    }
+    return maxSum;
 }
 
