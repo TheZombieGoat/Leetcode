1882d7997d9a35262a492edbd00188f216a2d14c
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-07 22:38:28 -0400

    Update climbing_stairs.py

--- Dynamic_Programming/climbing_stairs.py
+++ Dynamic_Programming/climbing_stairs.py
@@ -6,13 +6,7 @@ Each time you can either climb 1 or 2 steps. In how many distinct ways can you c
 
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        elif n == 2:
-            return 2
-        s = [0]*n
-        s[0] = 1
-        s[1] = 2
-        for i in range(2,n):
-            s[i] = s[i-2] + s[i-1]
-        return s[n-1]
+        i,j = 1,2
+        for _ in range(2,n):
+            i,j = j, i+j
+        return j if n > 1 else i
