b733c2a00750536a2adb49a2c411f96a04022228
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-19 22:50:05 -0400

    Update flood_fill.py
    passes more testcases. work in progress.

--- Graphs/flood_fill.py
+++ Graphs/flood_fill.py
@@ -4,24 +4,29 @@ class Solution:
         width = len(image[0])
         orig = image[sr][sc]
         helper(image,sr,sc,height,width,color,orig)
+        image[sr][sc] = color
         return image
 
 def helper(image,x,y,height,width,color,orig):
     #up
-    if y-1 >= 0 and image[x][y-1] == orig:
-        image[x][y-1] = color
-        helper(image,x,y-1,height,width,color,orig)
+    if y-1 >= 0 and image[y-1][x] == orig:
+        if orig != color:
+            image[y-1][x] = color
+            helper(image,x,y-1,height,width,color,orig)
     #down
-    if y+1 < height and image[x][y+1] == orig:
-        image[x][y+1] = color
-        helper(image,x,y+1,height,width,color,orig)
+    if y+1 < height and image[y+1][x] == orig:
+        if orig != color:
+            image[y+1][x] = color
+            helper(image,x,y+1,height,width,color,orig)
     #left
-    if x-1 >= 0 and image[x-1][y] == orig:
-        image[x-1][y] = color
-        helper(image,x-1,y,height,width,color,orig)
-    if x+1 < width and image[x+1][y] == orig:
-        image[x+1][y] = color
-        helper(image,x+1,y,height,width,color,orig) 
-    if image[x][y] != orig:
+    if x-1 >= 0 and image[y][x-1] == orig:
+        if orig != color:
+            image[y][x-1] = color
+            helper(image,x-1,y,height,width,color,orig)
+    #down
+    if x+1 < width and image[y][x+1] == orig:
+        if orig != color:
+            image[y][x+1] = color
+            helper(image,x+1,y,height,width,color,orig) 
+    if image[y][x] == color:
         return
-    return
