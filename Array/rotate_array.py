f1579ad93be7ac9b283595e811c3b65cc001c6ec
TheZombieGoat <98498075+TheZombieGoat@users.noreply.github.com>
2023-06-27 23:08:50 -0400

    Update rotate_array.py

--- Array/rotate_array.py
+++ Array/rotate_array.py
@@ -1,14 +1,7 @@
 # Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
-# O(n) time complexity, O(1) space complexity.
+# O(n) time complexity, O(n) space complexity.
 
 class Solution:
     def rotate(self, nums: List[int], k: int) -> None:
-        size = len(nums)
-        k  %= size
-        if k != 0:
-            nums.reverse() 
-            a = (size - k) // 2
-            for i in range(a):
-                nums[k+i],nums[-i-1] = nums[-i-1],nums[k+i]
-            for i in range(k//2):
-                nums[i],nums[k-i-1] = nums[k-i-1],nums[i]
+        k %= len(nums)
+        nums[:] = nums[-k:] + nums[:-k]
